e33c9e2bc44cae9d5dccc3b30b3461d78c5ee9a9
Dylan Cawsey <c3232539@uon.edu.au>
2019-09-03 09:21:22 +1000

    [UPDATE] web.xml

--- src/main/java/controllers/LoginAdmin.java
+++ src/main/java/controllers/LoginAdmin.java
@@ -38,7 +38,7 @@ public class LoginAdmin extends HttpServlet{
 
         if (username.equals(user) && password.equals(pass)) {
             context.setAttribute("adminUser", new AdminUser(username));
-            request.getRequestDispatcher("/adminView.jsp").forward(request,response);
+            request.getRequestDispatcher("/adminView.jsp").forward(request, response);
         } else {
             request.getRequestDispatcher("/adminLogin.jsp?status=failed").forward(request, response);
         }
